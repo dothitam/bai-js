d0a407fc2560819b6f319c5b4262a031660e7dbc
dothitam <dotam261205@gmail.com>
2024-10-02 16:35:24 +0700

    Add files via upload

---
+++ vidu.js
@@ -0,0 +1,79 @@
+alert("HELLO,ÁNH ĐƠ")
+let tong_so_hoc_sinh = "năm mươi"
+let hs_nam = 30
+let hs_nu = 20
+let message = ("Hôm nay là thứ tư,tôi phải học cả ngày")
+const so_ngay = 7
+console.log(tong_so_hoc_sinh)
+console.log(hs_nam)
+console.log(hs_nu)
+console.log(message)
+console.log(so_ngay)
+message = ("Thứ 6, học buổi chiều")
+console.log(message)
+console.log(typeof tong_so_hoc_sinh)
+document.write(18+3.5)
+document.write("<br>")
+document.write(2024+1969)
+document.write("<br>")
+document.write(69/96)
+document.write("<br>")
+document.write(0.6996*0.3)
+
+
+let a = 8
+if (a ==5) {
+    console.log('nam')
+}
+if (a ==6) {
+    console.log('sau')
+}
+if (a ==7) {
+    console.log('bay')
+}
+if (a ==8) {
+    console.log('tam')
+}
+if (a ==9) {
+    console.log('chin')
+}
+
+switch (a) {
+    case 5:
+        console.log('nam')
+        break;
+    case 6:
+        console.log('sau')
+        break;
+    case 7:
+        console.log('bay')
+        break;
+    case 8:
+         console.log('tam')
+         break;
+     case 9:
+         console.log('chin')
+         break;
+    
+}
+console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+console.log(5)
+console.log(6)
+console.log(7)
+console.log(8)
+console.log(9)
+console.log(10)
+
+let index = 0;
+for (index = 0; index < 10; index++) {
+    console.log(index);
+}
+let so_nguyen = 2000;
+if (so_nguyen % 2){
+    console.log ('so le')
+}else{
+    console.log('so chan')
+}
